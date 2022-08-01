8477584186e23717874aade5d3e5f46a35c13cbd
Richard Fu <richardfu98@gmail.com>
2022-07-31 22:41:26 -0500

    added a 5kyu challenge

---
+++ 5kyu/Number of trailing zeros of N!.js
@@ -0,0 +1,47 @@
+/*
+
+Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+N! = 1 * 2 * 3 *  ... * N
+
+Be careful 1000! has 2568 digits...
+
+For more info, see: http://mathworld.wolfram.com/Factorial.html
+
+Examples
+zeros(6) = 1
+# 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+
+zeros(12) = 2
+# 12! = 479001600 --> 2 trailing zeros
+Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.
+
+*/
+
+
+//my solution
+
+function zeros (n) {
+    let result = 0;
+    while (n = Math.floor(n / 5)){ 
+      result += n;
+    }
+    return result;
+}
+
+//best practice solution
+
+function zeros (n) {
+    var zs = 0;
+    while(n>0){
+      n=Math.floor(n/5);
+      zs+=n
+    }
+    return zs;
+  }
+
+//alternative solution
+
+function zeros(n) {
+    return n/5 < 1 ? 0 : Math.floor(n/5) + zeros(n/5);
+  }
